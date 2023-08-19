5ef68e0646e06686c031d3bc81aea20e3fe449d3
M2Ihsan <murnihsan9691@gmail.com>
2023-08-19 22:48:10 +0700

    membuat website naufaruqi matcha cafe

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika burger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// Klik di luar sidebar untuk menghilangkan NAV
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
